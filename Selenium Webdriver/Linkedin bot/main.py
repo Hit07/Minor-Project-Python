c2a3dc9617c798506f3dc2b9cd7d1a0fb87579bd
Hitesh N <hiteshnovember@gmail.com>
2023-10-30 08:55:11 +0530

    Require some changes

--- Selenium Webdriver/Linkedin bot/main.py
+++ Selenium Webdriver/Linkedin bot/main.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 ACCOUNT_EMAIL = ''
 ACCOUNT_PASSWORD = ''
@@ -34,13 +36,36 @@ password.send_keys(Keys.ENTER)
 # Confirmation By entering something that verification is complete
 input("Have you completed:")
 
+# applied = driver.find_element(By.CLASS_NAME, 'job-card-container__footer-job-state')
+# applied_text = applied.text
+# if applied_text:
+#     driver.refresh()
+#     click_first = driver.find_element(By.CSS_SELECTOR,'.scaffold-layout__list-container li')
+#     click_first.click()
+#     print("success")
+
+# list_of_jobs = driver.find_elements(By.CLASS_NAME,'scaffold-layout__list-container')
+# for job in list_of_jobs:
+#     print(job.text)
+#     if applied_text:
+#         pass
+
+
 # apply button
 time.sleep(3)
 apply_button = driver.find_element(By.CSS_SELECTOR, value=".jobs-s-apply button")
 apply_button.click()
 
-time.sleep(2)
-next_button = driver.find_element(By.CSS_SELECTOR, value='.ph5 pv4 button')
+time.sleep(3)
+next_button = driver.find_element(By.CSS_SELECTOR, value=' .display-flex button')
 next_button.click()
+next_button_1 = driver.find_element(By.CLASS_NAME, 'artdeco-button--primary')
+next_button_1.click()
+review = driver.find_element(By.CLASS_NAME, 'artdeco-button--primary')
+review.click()
+submit_button = driver.find_element(By.CLASS_NAME, 'artdeco-button--primary')
+submit_button.click()
+time.sleep(3)
+
 
 driver.quit()
